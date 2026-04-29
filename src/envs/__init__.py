68aa8b2c9f392494cae9477dc152a67192ebe59e
computationalorganizationalresearch <132207859+computationalorganizationalresearch@users.noreply.github.com>
2026-04-28 22:30:22 -0400

    Add arithmetic and grid planning verifiable environments

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -20,8 +20,13 @@ __all__ = [
     "Transition",
     "VerifiableEnvironment",
     "VerifyResult",
+    "ArithmeticExprEnv",
+    "GridPlanningEnv",
     "task_from_json",
     "task_to_json",
     "trace_from_json",
     "trace_to_json",
 ]
+
+from envs.arithmetic_expr import ArithmeticExprEnv
+from envs.grid_planning import GridPlanningEnv
